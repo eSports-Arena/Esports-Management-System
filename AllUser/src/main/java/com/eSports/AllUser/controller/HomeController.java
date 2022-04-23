9ed0b95037b78f795cbf3398512765dc6428db40
sovon singha <sovonsingha9@gmail.com>
2022-04-23 20:59:06 +0530

    homecontroller

--- AllUser/src/main/java/com/eSports/AllUser/controller/HomeController.java
+++ AllUser/src/main/java/com/eSports/AllUser/controller/HomeController.java
@@ -3,29 +3,27 @@ package com.eSports.AllUser.controller;
 import java.io.UnsupportedEncodingException;
 
 import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.eSports.AllUser.model.User;
 import com.eSports.AllUser.model.VerificationCode;
-import com.eSports.AllUser.service.ServiceUrl;
 import com.eSports.AllUser.service.SignupService;
 
 import net.bytebuddy.utility.RandomString;
 
-@Controller
+@RestController
 public class HomeController {
 
 	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
@@ -48,7 +46,7 @@ public class HomeController {
 	}
 	
 	@PostMapping(value = "/do-signup")
-	public String doSignup(@ModelAttribute("user") User user, HttpServletRequest request) throws UnsupportedEncodingException, MessagingException
+	public User doSignup(@RequestBody User user,@RequestHeader String requestUrl) throws UnsupportedEncodingException, MessagingException
 	{
 		log.debug(".........................inside doSignUp.........................");
 		System.out.println(signupService.saveUser(user));
@@ -58,9 +56,10 @@ public class HomeController {
 		VerificationCode verificationCode = signupService.saveVerificationCode(code);
 		if(verificationCode!=null)
 		{
-			signupService.sendVerificationMail(verificationCode, user, request);
+			signupService.sendVerificationMail(verificationCode, user, requestUrl);
 		}
-		return "SignUpSuccess";
+		User user2 = signupService.saveUser(user);
+		return user2;
 	}
 	
 	@GetMapping(value = "/verify/{verify_code}")
